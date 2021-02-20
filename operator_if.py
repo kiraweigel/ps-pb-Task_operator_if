5d83a96a9ed0b8cd82d3fe1e18d67b2c4cdf78d3
Anna <kiraweigel4660@gmail.com>
2021-02-20 17:06:38 +0300

    operator if

---
+++ operator_if.py
@@ -0,0 +1,21 @@
+cities = ['Москва', 'Париж', 'Лондон']
+
+users = [{'name': 'Иван', 'age': 35},
+         {'name': 'Мария', 'age': 22},
+         {'name': 'Соня', 'age': 20}]
+         
+tourists = [{'user': users[0], 'city': cities[2]},
+            {'user': users[1], 'city': cities[0]},
+            {'user': users[2], 'city': cities[1]}]
+
+city = input('Введите город: ').lower()
+
+if city in tourists[0]['city'].lower():
+    print(f"Турист {tourists[0]['user']['name']} возраст {tourists[0]['user']['age']}. Посетил город {tourists[0]['city']}")
+elif city in tourists[1]['city'].lower():
+    print(f"Турист {tourists[1]['user']['name']} возраст {tourists[1]['user']['age']}. Посетил город {tourists[1]['city']}")
+elif city in tourists[2]['city'].lower():
+    print(f"Турист {tourists[2]['user']['name']} возраст {tourists[2]['user']['age']}. Посетил город {tourists[2]['city']}")
+else:
+    print('Город не найден')
+
